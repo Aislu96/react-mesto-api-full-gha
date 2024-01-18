73647fea3aa8537dd50af76477670dcf8e27f199
Aislu Kharisova <arslanova1996ais@list.ru>
2024-01-18 14:29:44 +0300

    frontend

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,129 @@
+class Api {
+    #url;
+
+    constructor({url}) {
+        this.#url = url;
+    }
+
+    _sendRequest(url, options) {
+        return fetch(url, options)
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error('Что-то пошло не так...')
+            })
+    }
+
+    getCards() {
+        const token = localStorage.getItem('jwt');
+        return this._sendRequest(`${this.#url}/cards`, {
+            method: "GET",
+            headers: {
+                authorization: `Bearer ${token}`,
+                'Content-Type': "application/json"
+            }
+        });
+    }
+
+    getUser() {
+        const token = localStorage.getItem('jwt');
+        return this._sendRequest(`${this.#url}/users/me`, {
+            method: "GET",
+            headers: {
+                authorization: `Bearer ${token}`,
+                'Content-Type': "application/json"
+            },
+        });
+    }
+
+    patchUser(data) {
+        const token = localStorage.getItem('jwt');
+        return this._sendRequest(`${this.#url}/users/me`, {
+            method: 'PATCH',
+            headers: {
+                authorization: `Bearer ${token}`,
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify({
+                name: data.name,
+                about: data.about
+            })
+        });
+    }
+
+    postCard(data) {
+        const token = localStorage.getItem('jwt');
+        return this._sendRequest(`${this.#url}/cards`, {
+            method: 'POST',
+            headers: {
+                authorization: `Bearer ${token}`,
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify({
+                name: data.title,
+                link: data.link
+            })
+        })
+    }
+
+    deleteCard(id) {
+        const token = localStorage.getItem('jwt');
+        return this._sendRequest(`${this.#url}/cards/${id}`, {
+            method: 'DELETE',
+            headers: {
+                authorization: `Bearer ${token}`,
+                'Content-Type': "application/json"
+            },
+        });
+    }
+
+    likeCard(id) {
+        const token = localStorage.getItem('jwt');
+        return this._sendRequest(`${this.#url}/cards/${id}/likes`, {
+            method: 'PUT',
+            headers: {
+                authorization: `Bearer ${token}`,
+                'Content-Type': "application/json"
+            },
+        });
+    }
+
+    deleteLikeCard(id) {
+        const token = localStorage.getItem('jwt');
+        return this._sendRequest(`${this.#url}/cards/${id}/likes`, {
+            method: 'DELETE',
+            headers: {
+                authorization: `Bearer ${token}`,
+                'Content-Type': "application/json"
+            },
+        });
+    }
+
+    patchAvatar(data) {
+        const token = localStorage.getItem('jwt');
+        return this._sendRequest(`${this.#url}/users/me/avatar`, {
+            method: 'PATCH',
+            headers: {
+                authorization: `Bearer ${token}`,
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify({
+                avatar: data.avatar
+            })
+        });
+    }
+}
+
+// const api = new Api({
+//     url: 'https://mesto.nomoreparties.co/v1/cohort-75',
+//     headers: {
+//         authorization: '8f0f3959-562a-4d67-8672-647db07d1306',
+//         'Content-Type': "application/json"
+//     }
+// });
+const api = new Api({
+    url: 'http://localhost:3000',
+});
+
+export default api;
---
+++ frontend/src/utils/auth.js
@@ -0,0 +1,60 @@
+export const BASE_URL = 'http://localhost:3000';
+
+export const register = (email, password) => {
+    return fetch(`${BASE_URL}/signup`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({email, password})
+    }).then((response) => {
+        try {
+            if (response.status === 201) {
+                return response.json();
+            }
+        } catch (e) {
+            return (e)
+        }
+    })
+};
+
+
+export const login = (email, password) => {
+    return fetch(`${BASE_URL}/signin`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({email, password})
+    }).then((response) => {
+        try {
+            if (response.status === 200) {
+                return response.json();
+            }
+        } catch (e) {
+            return (e)
+        }
+    }).then((res) => {
+        if (res.token) {
+            localStorage.setItem('jwt', res.token);
+            return res;
+        } else {
+            return;
+        }
+    })
+};
+
+
+export const getContent = () => {
+    const token = localStorage.getItem('jwt');
+    return fetch(`${BASE_URL}/users/me`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        }
+    })
+        .then(res => res.json())
+        .then(data => data)
+}
